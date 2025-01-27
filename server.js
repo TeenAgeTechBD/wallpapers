0b08eb5a5cad2fe947b6cdd7d058a1c5f198928d
It'z RJ <faiadmahmudadil@gmail.com>
2025-01-27 15:43:44 +0600

    ...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
